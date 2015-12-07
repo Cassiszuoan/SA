966322de93c7d448571f36ec62ed2e24459a3727
Cassiszuoan <jerry831210@gmail.com>
2015-12-07 15:52:20 +0800

    Added <DAO>  Pay , Query , Register

---
+++ src/main/java/com/practice/webapp/dao/RegisterDAO.java
@@ -0,0 +1,13 @@
+package com.practice.webapp.dao;
+import java.util.List;
+
+import com.practice.webapp.entity.Examinee;
+
+public interface RegisterDAO {
+	
+	public void register(Examinee examinee);
+    public void setTestNumber(Examinee examinee);
+    public void setTestRoom(Examinee examinee);
+    public void setPayment(Examinee examinee);
+    public void setSubject(Examinee examinee);
+}
