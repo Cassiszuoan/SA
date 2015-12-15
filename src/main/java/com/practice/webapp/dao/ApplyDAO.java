a681d50295b0d383c561ca56655ff2c4211e69d3
Cassiszuoan <jerry831210@gmail.com>
2015-12-15 22:55:11 +0800

    ApplyDAO ,ApplyDAOImpl  model done

--- src/main/java/com/practice/webapp/dao/ApplyDAO.java
+++ src/main/java/com/practice/webapp/dao/ApplyDAO.java
@@ -1,5 +1,10 @@
 package com.practice.webapp.dao;
+import com.practice.webapp.entity.Examinee;
+import com.practice.webapp.entity.TestRoom;
 
 public interface ApplyDAO {
 
+	public void applyGSAT(Examinee examinee);
+	public void applyAST(Examinee examinee);
+	public void applyEL(Examinee examinee);
 }
--- src/main/java/com/practice/webapp/dao/impl/ApplyDAOImpl.java
+++ src/main/java/com/practice/webapp/dao/impl/ApplyDAOImpl.java
@@ -1,5 +1,48 @@
 package com.practice.webapp.dao.impl;
 
-public class ApplyDAOImpl {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import com.practice.webapp.dao.ApplyDAO;
+import com.practice.webapp.entity.Examinee;
+import com.practice.webapp.entity.Examinee;
+
+
+
+public class ApplyDAOImpl implements ApplyDAO {
+	
+	
+	private DataSource dataSource;
+	private Connection conn = null ;
+	private ResultSet rs = null ;
+	private PreparedStatement smt = null ;
+	
+	public void setDataSource(DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
+
+	@Override
+	public void applyGSAT(Examinee examinee) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void applyAST(Examinee examinee) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void applyEL(Examinee examinee) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
